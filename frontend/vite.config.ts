960daa85fa0c871872c93bcddef1a36703a3b14f
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2025-06-10 16:31:45 +0900

    feat: integrate Storybook and Vitest for enhanced UI testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,12 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isStorybook = process.argv[1]?.includes("storybook");
+
 export default defineConfig(({mode}) => ({
   base: mode === "production" ? "/model_rocket_simulator/" : undefined,
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    !isStorybook && reactRouter(), tsconfigPaths()],
   // server: {
   //   port: 5173,
   //   strictPort: true,
