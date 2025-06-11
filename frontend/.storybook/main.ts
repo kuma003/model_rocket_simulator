7dd4e09cd1f23d99a3b03f92c13c1012b82c58ab
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2025-06-11 10:21:25 +0900

    feat: update Storybook configuration to include PostCSS with Mantine presets and variables

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -2,19 +2,41 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../app/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+    '@storybook/addon-actions',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y'
   ],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
+  },
+  "viteFinal": async (config) => {
+    return {
+      ...config,
+      css: {
+        ...config.css,
+        postcss: {
+          plugins: [
+            require('postcss-preset-mantine'),
+            require('postcss-simple-vars')({
+              variables: {
+                'mantine-breakpoint-xs': '36em',
+                'mantine-breakpoint-sm': '48em',
+                'mantine-breakpoint-md': '62em',
+                'mantine-breakpoint-lg': '75em',
+                'mantine-breakpoint-xl': '88em',
+              },
+            }),
+          ],
+        },
+      },
+    };
   }
 };
 export default config;
