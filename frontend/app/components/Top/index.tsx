8edcfa0566ae2b6a7f2f2d50d97347c747da2afc
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2025-06-11 14:53:36 +0900

    feat: update MenuButton styles and integrate with Top component

--- frontend/app/components/Top/index.tsx
+++ frontend/app/components/Top/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from "./toppage.module.scss";
+import MenuButton from '../MenuButton';
+import { PlayArrow, Score, Settings } from '../Icons';
 
 const Top: React.FC = () => {
     return (
         <div className={styles.toppage}>
-            
+            <MenuButton
+            buttons={[{
+                label:"プレイ",
+                leftIcon:<PlayArrow fill="white" />,
+                onClick:() => console.log('Home button clicked')
+            },
+            {
+                label: "設定",
+                leftIcon: <Settings fill="white" />
+            },
+            {
+                label: "ランキング",
+                leftIcon: <Score fill="white" />
+            },
+        ]
+            }
+            />  
         </div>
     );
 };
--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -19,16 +19,6 @@ export const links: Route.LinksFunction = () => [
     href: "/favicon.ico",
     type: "image/x-icon",
   },
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
